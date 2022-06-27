b3e2f9269f2af107b694b4462f5d224791de38c5
Jared Collier <jared@Jareds-MBP.localdomain>
2022-06-27 11:20:06 -0600

    assessment part 2 step 4

--- server/controller.js
+++ server/controller.js
@@ -12,6 +12,18 @@ const sequelize = new Sequelize (CONNECTION_STRING, {
 })
 
 module.exports = {
+    getCountries: (req, res) => {
+        sequelize.query(`
+            SELECT name FROM countries;
+        `)
+        .then(dbRes =>
+            res.status(200).send(dbRes[0])
+        )
+        .catch(err => {
+            console.log(err)
+            res.status(500).send()
+        })
+    },
     seed: (req, res) => {
         sequelize.query(`
             drop table if exists cities;
